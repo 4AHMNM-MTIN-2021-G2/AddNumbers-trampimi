0ca63d9b15cc0473e3459d06be21400099c6b20a
trampimi <mira.trampitsch02@htl-salzburg.ac.at>
2021-06-02 15:52:19 +0200

    script angelegt und auf die Input Felder hinzugefuegt

---
+++ Assets/MyGame/Scripts/ControllerScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControllerScript : MonoBehaviour
+{
+
+    public InputField Wert_1;
+    public InputField Wert_2;
+    public Text Ergebnis;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void addieren()
+    {
+        var Wert_1_t = int.Parse(Wert_1.text);
+
+        var Wert_2_t = int.Parse(Wert_2.text);
+
+        var Wert_gesamt = Wert_1_t + Wert_2_t;
+
+        Ergebnis.text = Wert_gesamt.ToString();
+
+        Debug.Log(Wert_gesamt);
+
+    }
+}
